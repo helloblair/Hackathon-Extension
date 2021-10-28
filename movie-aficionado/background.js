e34aadb63ea4ef8cd04f439ccc35b4555f3e7475
Daniel <dannguyen1191@yahoo.com>
2021-10-28 13:42:37 -0500

    mission accomplished

--- movie-aficionado/background.js
+++ movie-aficionado/background.js
@@ -1,3 +1,3 @@
 chrome.browserAction.onClicked.addListener(function() {
-    chrome.tabs.create({'url':"chrome://newtab"})
+    chrome.tabs.create({'url':"https://www.youtube.com/embed/_hB7jWoryL4?autoplay=1"})
 })
