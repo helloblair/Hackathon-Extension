f282b66ac22ef15a068c0c15ef0f512ce30a06e2
Daniel <dannguyen1191@yahoo.com>
2021-10-27 20:50:06 -0500

    added a bunch of shit

--- chrome-extension/popup.js
+++ chrome-extension/popup.js
@@ -1,20 +1,45 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var checkPageButton = document.getElementById('checkPage');
-  checkPageButton.addEventListener('click', function() {
+// document.addEventListener('DOMContentLoaded', function() {
+//   var checkPageButton = document.getElementById('checkPage');
+//   checkPageButton.addEventListener('click', function() {
 
-    chrome.tabs.getSelected(null, function(tab) {
-      d = document;
+//     chrome.tabs.getSelected(null, function(tab) {
+//       d = document;
 
-      var f = d.createElement('form');
-      f.action = 'http://gtmetrix.com/analyze.html?bm';
-      f.method = 'post';
-      var i = d.createElement('input');
-      i.type = 'hidden';
-      i.name = 'url';
-      i.value = tab.url;
-      f.appendChild(i);
-      d.body.appendChild(f);
-      f.submit();
-    });
-  }, false);
-}, false);
+//       var f = d.createElement('form');
+//       f.action = 'http://gtmetrix.com/analyze.html?bm';
+//       f.method = 'post';
+//       var i = d.createElement('input');
+//       i.type = 'hidden';
+//       i.name = 'url';
+//       i.value = tab.url;
+//       f.appendChild(i);
+//       d.body.appendChild(f);
+//       f.submit();
+//     });
+//   }, false);
+// }, false);
+
+// Initialize button with user's preferred color
+let changeColor = document.getElementById("changeColor");
+
+chrome.storage.sync.get("color", ({ color }) => {
+  changeColor.style.backgroundColor = color;
+});
+
+// When the button is clicked, inject setPageBackgroundColor into current page
+changeColor.addEventListener("click", async () => {
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    function: setPageBackgroundColor,
+  });
+});
+
+// The body of this function will be executed as a content script inside the
+// current page
+function setPageBackgroundColor() {
+  chrome.storage.sync.get("color", ({ color }) => {
+    document.body.style.backgroundColor = color;
+  });
+}
