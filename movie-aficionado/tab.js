6cb0ce7d42bb9c6ee299852d7486140ed8d74170
Daniel <dannguyen1191@yahoo.com>
2021-10-27 23:56:38 -0500

    new folder new me

---
+++ movie-aficionado/tab.js
@@ -0,0 +1,93 @@
+// let page = document.getElementById("buttonDiv");
+// let selectedClassName = "current";
+
+// // Reacts to a button click by marking the selected button and saving
+// // the selection
+// function handleButtonClick(event) {
+//   // Remove styling from the previously selected color
+//   let current = event.target.parentElement.querySelector(
+//     `.${selectedClassName}`
+//   );
+//   if (current && current !== event.target) {
+//     current.classList.remove(selectedClassName);
+//   }
+
+//   // Mark the button as selected
+//   let color = event.target.dataset.color;
+//   event.target.classList.add(selectedClassName);
+//   chrome.storage.sync.set({ color });
+// }
+
+// // Add a button to the page for each supplied color
+// function constructOptions(buttonColors) {
+//   chrome.storage.sync.get("color", (data) => {
+//     let currentColor = data.color;
+//     // For each color we were provided…
+//     for (let buttonColor of buttonColors) {
+//       // …create a button with that color…
+//       let button = document.createElement("button");
+//       button.dataset.color = buttonColor;
+//       button.style.backgroundColor = buttonColor;
+
+//       // …mark the currently selected color…
+//       if (buttonColor === currentColor) {
+//         button.classList.add(selectedClassName);
+//       }
+
+//       // …and register a listener for when that button is clicked
+//       button.addEventListener("click", handleButtonClick);
+//       page.appendChild(button);
+//     }
+//   });
+// }
+
+// // Initialize the page by constructing the color options
+// constructOptions(presetButtonColors);
+
+tab.Core = function() {
+  // quote controller
+  this.quoteCtrl = new tab.QuoteCtrl();
+}
+
+tab.Core.prototype = {
+  // `setQuote` method
+	// This method should set the `quoteStr` property of the momentum core. This method will be used as the callback for quoteCtrl's `fetchQuote` function.
+	// 
+	// hint. check out the `Date` object! Use `getHours` and `getMinutes`.
+	// hint. figure out what kind of response the quoteData is going to be, and see how you might be able to access the quote of the day from that.
+  setQuote: function(quoteData) {
+		this.quoteStr = quoteData.message;
+		this.quoteEl.text(this.quoteStr);
+		this.render();
+  },
+
+  // `updateQuote` method
+	// This function should call quoteCtrl.fetchQuote and pass in this.setQuote as the callback.
+	//
+	// note. you might run into scoping issues again. You should know how to solve them by now, using .call, .apply, or .bind.
+	updateQuote: function() {
+		// YOUR CODE HERE
+		this.quoteCtrl.fetchQuote(this.setQuote.bind(this));
+
+	},
+
+  // `start` method
+	// This method will call some of the `update...` methods. This function will be called when the page has finished loading, so that Momentum can start off with the more up-to-date data.
+	start: function() {
+
+		function error() {
+		  throw "Error occured!";
+		};
+		
+		this.updateQuote();
+		this.render();
+	},
+
+  // `render` method
+	// This method should "render" the time, quote and weather strings on your page by replacing the text value of your elements with their respective properties.
+	// ex. this.timeStr will be rendered on to the screen using this.timeEl.text(this.timeStr);
+  render: function() {
+		// YOUR CODE HERE
+		this.quoteEl.text(this.quoteStr);
+  }
+}
